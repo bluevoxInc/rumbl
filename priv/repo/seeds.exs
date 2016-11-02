009f3d56f497af9c2c381b516cf7554b8ef039db
wnorman <bluevox.inc@gmail.com>
2016-11-02 14:44:00 -0400

    add chapter 7 code, Video Categories

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,13 +10,18 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-alias Rumbl.User
+#alias Rumbl.User
 alias Rumbl.Repo
+alias Rumbl.Category
 
-Repo.insert(%User{name: "Jose", username: "josevalim", password_hash: "<3<elixir"})
-Repo.insert(%User{name: "Bruce", username: "redrapids", password_hash: "7langs"})
-Repo.insert(%User{name: "Chris", username: "cmccord", password_hash: "phoenix"})
+#Repo.insert(%User{name: "Jose", username: "josevalim", password_hash: "<3<elixir"})
+#Repo.insert(%User{name: "Bruce", username: "redrapids", password_hash: "7langs"})
+#Repo.insert(%User{name: "Chris", username: "cmccord", password_hash: "phoenix"})
 
-for u <- Repo.all(User) do 
-  Repo.update!(User.registration_changeset(u, %{password: u.password_hash || "temppass"}))
+#for u <- Repo.all(User) do 
+#Repo.update!(User.registration_changeset(u, %{password: u.password_hash || "temppass"}))
+#end
+
+for category <- ~w(Action Drama Romance Comedy Sci-fi) do
+  Repo.get_by(Category, name: category) || Repo.insert!(%Category{name: category})
 end
