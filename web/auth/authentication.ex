5a440b1a2ed1445bd442885bd463f97bb3c8c3f2
wnorman <bluevox.inc@gmail.com>
2016-11-20 12:54:22 -0500

    add ch10 code

--- web/auth/authentication.ex
+++ web/auth/authentication.ex
@@ -10,12 +10,13 @@ defmodule Rumbl.Authentication do
 
   def call(conn, repo) do
     user_id = get_session(conn, :user_id)
+
     cond do
     #note the first condition accepts a user value if it's assigned in :current_user regardless of how it got there. This is to facilitate testing (see page 137). I am not so sure about the security of this for a production application.
       user = conn.assigns[:current_user] ->
-        conn
+        put_current_user(conn, user)
       user = user_id && repo.get(Rumbl.User, user_id) ->
-        assign(conn, :current_user, user)
+        put_current_user(conn, user)
       true ->
         assign(conn, :current_user, nil)
     end
@@ -23,9 +24,17 @@ defmodule Rumbl.Authentication do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 
   def login_by_username_and_pass(conn, username, given_pass, opts) do
